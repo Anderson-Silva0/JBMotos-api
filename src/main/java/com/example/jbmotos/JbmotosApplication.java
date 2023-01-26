739061553ed759ccab65d754c806e590f5d865d9
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-01-26 20:38:04 -0300

    Projeto iniciado

---
+++ src/main/java/com/example/jbmotos/JbmotosApplication.java
@@ -0,0 +1,13 @@
+package com.example.jbmotos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbmotosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JbmotosApplication.class, args);
+    }
+
+}
