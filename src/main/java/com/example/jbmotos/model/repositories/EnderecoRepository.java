a1332cc35b07bf869fe6973ff76d1ab71eb3a3d6
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-01-26 20:48:31 -0300

    interface EnderecoRepository criada

---
+++ src/main/java/com/example/jbmotos/model/repositories/EnderecoRepository.java
@@ -0,0 +1,10 @@
+package com.example.jbmotos.model.repositories;
+
+import com.example.jbmotos.model.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}
