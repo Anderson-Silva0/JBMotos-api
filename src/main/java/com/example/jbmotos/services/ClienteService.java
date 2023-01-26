81203162564b179df6b02da8fa58a5b5553bec36
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-01-26 20:53:33 -0300

    interface ClienteService criada

---
+++ src/main/java/com/example/jbmotos/services/ClienteService.java
@@ -0,0 +1,20 @@
+package com.example.jbmotos.services;
+
+import com.example.jbmotos.api.dto.ClienteDTO;
+import com.example.jbmotos.model.entity.Cliente;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ClienteService {
+
+    Cliente salvarCliente(ClienteDTO clienteDTO);
+
+    List<Cliente> buscarTodosClientes();
+
+    Optional<Cliente> buscarClientePorCPF(String cpf);
+
+    Cliente atualizarCliente(ClienteDTO clienteDTO);
+
+    void deletarCliente(String cpf);
+}
