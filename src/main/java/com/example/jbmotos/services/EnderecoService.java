c0f971569b548e82c42bf7c52c7b10d66761d861
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-01-26 20:53:54 -0300

    interface EnderecoService criada

---
+++ src/main/java/com/example/jbmotos/services/EnderecoService.java
@@ -0,0 +1,19 @@
+package com.example.jbmotos.services;
+
+import com.example.jbmotos.api.dto.EnderecoDTO;
+import com.example.jbmotos.model.entity.Endereco;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EnderecoService {
+    Endereco salvarEndereco(EnderecoDTO enderecoDTO);
+
+    List<Endereco> buscarTodosEnderecos();
+
+    Optional<Endereco> buscarEnderecoPorId(Integer id);
+
+    Endereco atualizarEndereco(EnderecoDTO enderecoDTO);
+
+    void deletarEndereco(Integer id);
+}
