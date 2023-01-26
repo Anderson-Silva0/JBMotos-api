2c95190c2b9a03e7f307b99a8d833ae6e32078ae
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-01-26 20:52:05 -0300

    excecao ObjetoNaoEncontradoException criada

---
+++ src/main/java/com/example/jbmotos/services/exception/ObjetoNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.example.jbmotos.services.exception;
+
+public class ObjetoNaoEncontradoException extends RuntimeException {
+    public ObjetoNaoEncontradoException(String msg) {
+        super(msg);
+    }
+}
