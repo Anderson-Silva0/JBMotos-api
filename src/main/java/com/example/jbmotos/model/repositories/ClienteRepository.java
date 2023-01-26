29199318b5c1976d3436b92165a96ab1bd452fed
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-01-26 20:48:04 -0300

    interface ClienteRepository criada

---
+++ src/main/java/com/example/jbmotos/model/repositories/ClienteRepository.java
@@ -0,0 +1,14 @@
+package com.example.jbmotos.model.repositories;
+
+import com.example.jbmotos.model.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, String> {
+    Optional<Cliente> findClienteByCpf(String cpf);
+
+    void deleteClienteByCpf(String cpf);
+}
