270fb0a7ba3985bbeb590e854ac8a1a11803cf5c
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-01-26 20:50:48 -0300

    interface ProdutoRepository criada

---
+++ src/main/java/com/example/jbmotos/model/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.example.jbmotos.model.repositories;
+
+import com.example.jbmotos.model.entity.Produto;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
