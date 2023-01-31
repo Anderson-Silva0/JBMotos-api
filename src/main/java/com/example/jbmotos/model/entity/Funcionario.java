d7088f7e6b1fc1229d7234c0e5511834048e9a04
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-01-30 22:09:03 -0300

    alteracao em entity Funcionario

--- src/main/java/com/example/jbmotos/model/entity/Funcionario.java
+++ src/main/java/com/example/jbmotos/model/entity/Funcionario.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.br.CPF;
 
 import javax.persistence.*;
@@ -24,9 +25,11 @@ public class Funcionario {
 
     @Column(name = "nome")
     @NotBlank(message = "O campo Nome é obrigatório")
+    @Length(min = 3, max = 50, message = "O campo Nome deve ter entre 3 e 50 caracteres.")
     private String nome;
 
     @Column(name = "telefone")
     @NotBlank(message = "O campo Telefone é obrigatório")
+    @Length(min = 11, max = 15, message = "O campo Telefone deve ter entre 11 e 15 caracteres.")
     private String telefone;
 }
