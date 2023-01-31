4b8d1ed7f83ce35de87eb9c5ba0d4e428e9ad27f
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-01-30 22:02:19 -0300

    alteracao em EnderecoDTO

--- src/main/java/com/example/jbmotos/api/dto/EnderecoDTO.java
+++ src/main/java/com/example/jbmotos/api/dto/EnderecoDTO.java
@@ -16,18 +16,17 @@ public class EnderecoDTO {
     private Integer id;
 
     @NotBlank(message = "O campo Rua é obrigatório")
-    @Length(max = 100)
+    @Length(max = 100, message = "O campo Rua deve ter no máximo 100 caracteres.")
     private String rua;
 
     @NotBlank(message = "O campo CEP é obrigatório")
-    @Length(max = 10)
+    @Length(max = 10, message = "O campo CEP deve ter no máximo 10 caracteres.")
     private String cep;
 
     @NotBlank(message = "O campo Numero é obrigatório")
-    @Length(max = 5)
-    private String numero;
+    private Integer numero;
 
     @NotBlank(message = "O campo Cidade é obrigatório")
-    @Length(max = 50)
+    @Length(max = 50, message = "O campo Cidade deve ter no máximo 50 caracteres.")
     private String cidade;
 }
