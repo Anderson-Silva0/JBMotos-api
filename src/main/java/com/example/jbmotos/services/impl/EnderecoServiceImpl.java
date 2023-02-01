379e32287d82ea457082b08d209accac1201db32
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-02-01 16:19:50 -0300

    método verificaSeFuncionarioOuClienteExiste criado

--- src/main/java/com/example/jbmotos/services/impl/EnderecoServiceImpl.java
+++ src/main/java/com/example/jbmotos/services/impl/EnderecoServiceImpl.java
@@ -1,9 +1,15 @@
 package com.example.jbmotos.services.impl;
 
 import com.example.jbmotos.api.dto.EnderecoDTO;
+import com.example.jbmotos.model.entity.Cliente;
 import com.example.jbmotos.model.entity.Endereco;
+import com.example.jbmotos.model.entity.Funcionario;
+import com.example.jbmotos.model.enums.TipoUsuario;
 import com.example.jbmotos.model.repositories.EnderecoRepository;
+import com.example.jbmotos.services.ClienteService;
 import com.example.jbmotos.services.EnderecoService;
+import com.example.jbmotos.services.FuncionarioService;
+import com.example.jbmotos.services.exception.ObjetoNaoEncontradoException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,11 +26,18 @@ public class EnderecoServiceImpl implements EnderecoService {
     private EnderecoRepository enderecoRepository;
 
     @Autowired
+    private ClienteService clienteService;
+
+    @Autowired
+    private FuncionarioService funcionarioService;
+
+    @Autowired
     private ModelMapper mapper;
 
     @Override
     @Transactional
     public Endereco salvarEndereco(EnderecoDTO enderecoDTO) {
+        verificaSeFuncionarioOuClienteExiste(enderecoDTO);
         return enderecoRepository.save(mapper.map(enderecoDTO, Endereco.class));
     }
 
@@ -51,5 +64,19 @@ public class EnderecoServiceImpl implements EnderecoService {
     @Transactional
     public void deletarEndereco(Integer id) {
         enderecoRepository.deleteById(id);
+    }
+
+    public void verificaSeFuncionarioOuClienteExiste(EnderecoDTO enderecoDTO) {
+        if (TipoUsuario.valueOf(enderecoDTO.getTipoUsuario()) == TipoUsuario.FUNCIONARIO) {
+            Optional<Funcionario> funcionario = funcionarioService
+                    .buscarFuncionarioPorCPF(enderecoDTO.getCpfUsuario());
+            funcionario.orElseThrow(() -> new ObjetoNaoEncontradoException("Erro ao tentar salvar o Endereço. " +
+                    "Funcionário não cadastrado, ou CPF inválido."));
+        } else if (TipoUsuario.valueOf(enderecoDTO.getTipoUsuario()) == TipoUsuario.CLIENTE) {
+            Optional<Cliente> cliente = clienteService.
+                    buscarClientePorCPF(enderecoDTO.getCpfUsuario());
+            cliente.orElseThrow(() -> new ObjetoNaoEncontradoException("Erro ao tentar salvar o Endereço. " +
+                    "Cliente não cadastrado, ou CPF inválido."));
+        }
     }
 }
