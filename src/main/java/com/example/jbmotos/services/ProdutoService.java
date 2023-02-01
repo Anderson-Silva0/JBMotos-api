6b8b7a128564d7ef9bbd2b70cb398551ef76c834
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-01-31 22:08:38 -0300

    Interface ProdutoService finalizada

---
+++ src/main/java/com/example/jbmotos/services/ProdutoService.java
@@ -0,0 +1,20 @@
+package com.example.jbmotos.services;
+
+import com.example.jbmotos.api.dto.ProdutoDTO;
+import com.example.jbmotos.model.entity.Produto;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProdutoService {
+
+    Produto salvarProduto(ProdutoDTO produtoDTO);
+
+    List<Produto> buscarTodosProdutos();
+
+    Optional<Produto> buscarProdutoPorId(Integer id);
+
+    Produto atualizarProduto(ProdutoDTO produtoDTO);
+
+    void deletarProduto(Integer id);
+}
