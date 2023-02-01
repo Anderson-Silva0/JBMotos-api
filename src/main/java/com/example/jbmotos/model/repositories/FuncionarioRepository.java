87cb1485597a48be48f4e83b407b9cc6aef64125
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-01-31 22:42:55 -0300

    adicionando métodos em FuncionarioRepository

--- src/main/java/com/example/jbmotos/model/repositories/FuncionarioRepository.java
+++ src/main/java/com/example/jbmotos/model/repositories/FuncionarioRepository.java
@@ -4,7 +4,11 @@ import com.example.jbmotos.model.entity.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FuncionarioRepository extends JpaRepository<Funcionario, String> {
+    Optional<Funcionario> findFuncionarioByCpf(String cpf);
 
+    void deleteFuncionarioByCpf(String cpf);
 }
