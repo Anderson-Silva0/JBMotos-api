6cb0b48f91449577ad09ce60f039db878a6caf96
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-02-01 16:15:01 -0300

    metodo de salvarEndereco e metodo de excecao criado

--- src/main/java/com/example/jbmotos/api/controller/EnderecoController.java
+++ src/main/java/com/example/jbmotos/api/controller/EnderecoController.java
@@ -1,11 +1,45 @@
 package com.example.jbmotos.api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.jbmotos.api.dto.EnderecoDTO;
+import com.example.jbmotos.services.EnderecoService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/endereco")
 public class EnderecoController {
 
+    @Autowired
+    private EnderecoService enderecoService;
 
+    @Autowired
+    private ModelMapper mapper;
+
+    @PostMapping
+    public ResponseEntity<EnderecoDTO> salvarEndereco(@Valid @RequestBody EnderecoDTO enderecoDTO) {
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}").buildAndExpand( enderecoService.salvarEndereco(enderecoDTO) ).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public List<String> handleValidationException(MethodArgumentNotValidException ex) {
+        List<String> message = new ArrayList<>();
+        ex.getBindingResult().getAllErrors().stream().forEach( erro -> {
+            message.add(erro.getDefaultMessage());
+        });
+        return message;
+    }
 }
