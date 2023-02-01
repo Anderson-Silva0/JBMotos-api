3c081e689177d37bbda0fed9adaf62f58d15e31d
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-02-01 16:42:35 -0300

    metodo para interceptar exceções de validações criado

--- src/main/java/com/example/jbmotos/api/controller/ClienteController.java
+++ src/main/java/com/example/jbmotos/api/controller/ClienteController.java
@@ -5,15 +5,16 @@ import com.example.jbmotos.model.entity.Cliente;
 import com.example.jbmotos.services.ClienteService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/cliente")
@@ -31,5 +32,15 @@ public class ClienteController {
         URI uri = ServletUriComponentsBuilder
                 .fromCurrentRequest().buildAndExpand(cliente).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public List<String> handleValidationException(MethodArgumentNotValidException ex) {
+        List<String> mensagens = new ArrayList<>();
+        ex.getBindingResult().getAllErrors().stream().forEach( erro -> {
+            mensagens.add(erro.getDefaultMessage());
+        });
+        return mensagens;
     }
 }
