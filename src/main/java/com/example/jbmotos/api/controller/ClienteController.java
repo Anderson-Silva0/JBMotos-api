246750da6c1ad248414afae8e96fdc4a664ce10e
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-02-01 16:39:31 -0300

    metodo salvarCliente criado

--- src/main/java/com/example/jbmotos/api/controller/ClienteController.java
+++ src/main/java/com/example/jbmotos/api/controller/ClienteController.java
@@ -1,11 +1,35 @@
 package com.example.jbmotos.api.controller;
 
+import com.example.jbmotos.api.dto.ClienteDTO;
+import com.example.jbmotos.model.entity.Cliente;
+import com.example.jbmotos.services.ClienteService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/cliente")
 public class ClienteController {
 
+    @Autowired
+    private ClienteService clienteService;
 
+    @Autowired
+    private ModelMapper mapper;
+
+    @PostMapping
+    public ResponseEntity<ClienteDTO> salvarCliente(@Valid @RequestBody ClienteDTO clienteDTO) {
+       Cliente cliente = clienteService.salvarCliente(clienteDTO);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest().buildAndExpand(cliente).toUri();
+        return ResponseEntity.created(uri).build();
+    }
 }
