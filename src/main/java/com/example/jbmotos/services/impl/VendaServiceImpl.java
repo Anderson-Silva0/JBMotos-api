34ace7175bdd3b594a4e72dbc8038292ef73eb92
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-01-31 21:52:52 -0300

    classe VendaServiceImpl finalizada

--- src/main/java/com/example/jbmotos/services/impl/VendaServiceImpl.java
+++ src/main/java/com/example/jbmotos/services/impl/VendaServiceImpl.java
@@ -2,35 +2,47 @@ package com.example.jbmotos.services.impl;
 
 import com.example.jbmotos.api.dto.VendaDTO;
 import com.example.jbmotos.model.entity.Venda;
+import com.example.jbmotos.model.repositories.VendaRepository;
 import com.example.jbmotos.services.VendaService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
-
+@Service
 public class VendaServiceImpl implements VendaService {
 
+    @Autowired
+    private VendaRepository vendaRepository;
+
+    @Autowired
+    private ModelMapper mapper;
+
     @Override
-    public Venda salvarVenda(VendaDTO VendaDTO) {
-        return null;
+    public Venda salvarVenda(VendaDTO vendaDTO) {
+        return vendaRepository.save(mapper.map(vendaDTO, Venda.class));
     }
 
     @Override
     public List<Venda> buscarTodasVendas() {
-        return null;
+        return vendaRepository.findAll();
     }
 
     @Override
     public Optional<Venda> buscarVendaPorId(Integer id) {
-        return Optional.empty();
+        return vendaRepository.findById(id);
     }
 
     @Override
-    public Venda atualizarVenda(VendaDTO VendaDTO) {
-        return null;
+    public Venda atualizarVenda(VendaDTO vendaDTO) {
+        Objects.requireNonNull(vendaDTO.getId(), "Erro ao tentar atualizar a Venda. Informe um Id.");
+        return vendaRepository.save(mapper.map(vendaDTO, Venda.class));
     }
 
     @Override
     public void deletarVenda(Integer id) {
-
+        vendaRepository.deleteById(id);
     }
 }
