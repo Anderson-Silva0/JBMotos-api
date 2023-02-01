28ba881bd4130d2e5234b7783dfaf8324fae7f0b
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-01-31 23:14:26 -0300

    adicionando métodos em FornecedorRepository

--- src/main/java/com/example/jbmotos/model/repositories/FornecedorRepository.java
+++ src/main/java/com/example/jbmotos/model/repositories/FornecedorRepository.java
@@ -4,7 +4,12 @@ import com.example.jbmotos.model.entity.Fornecedor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FornecedorRepository extends JpaRepository<Fornecedor, Integer> {
 
+    Optional<Fornecedor> findFornecedorByCnpj(String cnpj);
+
+    void deleteFornecedorByCnpj(String cnpj);
 }
