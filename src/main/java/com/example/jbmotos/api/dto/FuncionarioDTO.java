72f356aadc04aec9120eade6a18edbda3bb40b11
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-01-31 22:40:28 -0300

    classe FuncionarioDTO finalizada

---
+++ src/main/java/com/example/jbmotos/api/dto/FuncionarioDTO.java
@@ -0,0 +1,28 @@
+package com.example.jbmotos.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FuncionarioDTO {
+
+    @CPF
+    private String cpf;
+
+    @NotBlank(message = "O campo Nome é obrigatório")
+    @Length(min = 3, max = 50, message = "O campo Nome deve ter entre 3 e 50 caracteres.")
+    private String nome;
+
+    @NotBlank(message = "O campo Telefone é obrigatório")
+    @Length(min = 11, max = 15, message = "O campo Telefone deve ter entre 11 e 15 caracteres.")
+    private String telefone;
+}
