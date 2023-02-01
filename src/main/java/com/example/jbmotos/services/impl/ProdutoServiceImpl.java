2decf4a3b131e85515acd3af5fe245f75b7aa6fb
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-01-31 22:29:09 -0300

    adicionando anotações @Transactional

--- src/main/java/com/example/jbmotos/services/impl/ProdutoServiceImpl.java
+++ src/main/java/com/example/jbmotos/services/impl/ProdutoServiceImpl.java
@@ -7,6 +7,7 @@ import com.example.jbmotos.services.ProdutoService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Objects;
@@ -22,27 +23,32 @@ public class ProdutoServiceImpl implements ProdutoService {
     private ModelMapper mapper;
 
     @Override
+    @Transactional
     public Produto salvarProduto(ProdutoDTO produtoDTO) {
         return produtoRepository.save(mapper.map(produtoDTO, Produto.class));
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Produto> buscarTodosProdutos() {
         return produtoRepository.findAll();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Optional<Produto> buscarProdutoPorId(Integer id) {
         return produtoRepository.findById(id);
     }
 
     @Override
+    @Transactional
     public Produto atualizarProduto(ProdutoDTO produtoDTO) {
         Objects.requireNonNull(produtoDTO.getId(), "Erro ao tentar atualizar o Produto. Informe um Id.");
         return produtoRepository.save(mapper.map(produtoDTO, Produto.class));
     }
 
     @Override
+    @Transactional
     public void deletarProduto(Integer id) {
         produtoRepository.deleteById(id);
     }
--- src/main/java/com/example/jbmotos/services/impl/VendaServiceImpl.java
+++ src/main/java/com/example/jbmotos/services/impl/VendaServiceImpl.java
@@ -7,6 +7,7 @@ import com.example.jbmotos.services.VendaService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Objects;
@@ -21,27 +22,32 @@ public class VendaServiceImpl implements VendaService {
     private ModelMapper mapper;
 
     @Override
+    @Transactional
     public Venda salvarVenda(VendaDTO vendaDTO) {
         return vendaRepository.save(mapper.map(vendaDTO, Venda.class));
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Venda> buscarTodasVendas() {
         return vendaRepository.findAll();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Optional<Venda> buscarVendaPorId(Integer id) {
         return vendaRepository.findById(id);
     }
 
     @Override
+    @Transactional
     public Venda atualizarVenda(VendaDTO vendaDTO) {
         Objects.requireNonNull(vendaDTO.getId(), "Erro ao tentar atualizar a Venda. Informe um Id.");
         return vendaRepository.save(mapper.map(vendaDTO, Venda.class));
     }
 
     @Override
+    @Transactional
     public void deletarVenda(Integer id) {
         vendaRepository.deleteById(id);
     }
