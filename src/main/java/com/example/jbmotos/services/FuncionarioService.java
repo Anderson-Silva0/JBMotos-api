5a6e65238875c0bc4ce842f5bcf79193b9fa20ad
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-01-31 22:41:25 -0300

    interface FuncionarioService finalizada

---
+++ src/main/java/com/example/jbmotos/services/FuncionarioService.java
@@ -0,0 +1,20 @@
+package com.example.jbmotos.services;
+
+import com.example.jbmotos.api.dto.FuncionarioDTO;
+import com.example.jbmotos.model.entity.Funcionario;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FuncionarioService {
+
+    Funcionario salvarFuncionario(FuncionarioDTO funcionarioDTO);
+
+    List<Funcionario> buscarTodosFuncionarios();
+
+    Optional<Funcionario> buscarFuncionarioPorCPF(String cpf);
+
+    Funcionario atualizarFuncionario(FuncionarioDTO funcionarioDTO);
+
+    void deletarFuncionario(String cpf);
+}
