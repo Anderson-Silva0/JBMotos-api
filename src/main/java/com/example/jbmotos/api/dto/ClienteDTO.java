9d7ab4d428ab5607fdc7ae777f88562ab403624a
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-02-03 14:19:30 -0300

    Cliente e ClienteDTO alterado

--- src/main/java/com/example/jbmotos/api/dto/ClienteDTO.java
+++ src/main/java/com/example/jbmotos/api/dto/ClienteDTO.java
@@ -1,5 +1,6 @@
 package com.example.jbmotos.api.dto;
 
+import com.example.jbmotos.model.entity.Endereco;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,6 +8,8 @@ import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.br.CPF;
 
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
@@ -16,19 +19,21 @@ import javax.validation.constraints.NotBlank;
 @Builder
 public class ClienteDTO {
 
-    @CPF
+    @CPF(message = "Número do CPF inválido")
     private String cpf;
 
     @NotBlank(message = "O campo Nome é obrigatório")
-    @Length(min = 3,max = 50)
+    @Length(min = 3,max = 50, message = "O campo Nome deve ter entre 3 e 50 caracteres")
     private String nome;
 
     @NotBlank(message = "O campo Email é obrigatório")
-    @Length(min =10, max = 50)
-    @Email
+    @Email(message = "Endereço de E-mail inválido")
     private String email;
 
     @NotBlank(message = "O campo Telefone é obrigatório")
-    @Length(min =11, max = 15, message = "O campo Telefone deve ter entre 11 e 15 caracteres.")
+    @Length(min = 9, max = 15, message = "O campo Telefone deve ter entre 9 e 15 caracteres.")
     private String telefone;
+
+    @NotBlank(message = "O campo Endereço é obrigatório")
+    private Integer endereco;
 }
--- src/main/java/com/example/jbmotos/model/entity/Cliente.java
+++ src/main/java/com/example/jbmotos/model/entity/Cliente.java
@@ -11,7 +11,7 @@ import org.hibernate.validator.constraints.br.CPF;
 
 import javax.persistence.*;
 
-@Table(name = "cliente", schema = "jbmotos")
+@Table(name = "cliente", schema = "jbmotos", uniqueConstraints = {@UniqueConstraint(columnNames = {"cpf"})})
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,23 +20,19 @@ import javax.persistence.*;
 public class Cliente {
 
     @Id
-    @Column(name = "cpf", unique = true)
-    @CPF
+    @Column(name = "cpf")
     private String cpf;
 
     @Column(name = "nome")
-    @NotBlank(message = "O campo Nome é obrigatório")
-    @Length(min = 3,max = 50)
     private String nome;
 
-    @Column(name = "email", unique = true)
-    @NotBlank(message = "O campo Email é obrigatório")
-    @Length(min =10, max = 50)
-    @Email
+    @Column(name = "email")
     private String email;
 
     @Column(name = "telefone")
-    @NotBlank(message = "O campo Telefone é obrigatório")
-    @Length(min =11, max = 15, message = "O campo Telefone deve ter entre 11 e 15 caracteres.")
     private String telefone;
+
+    @JoinColumn(name = "id_endereco")
+    @OneToOne(targetEntity = Endereco.class)
+    private Endereco endereco;
 }
