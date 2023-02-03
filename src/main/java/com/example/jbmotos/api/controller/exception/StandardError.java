cc07fc2b539c2d0057ac8cb043868e75430fb88d
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-02-03 16:32:47 -0300

    classe StandardError criada

---
+++ src/main/java/com/example/jbmotos/api/controller/exception/StandardError.java
@@ -0,0 +1,16 @@
+package com.example.jbmotos.api.controller.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Builder
+public class StandardError {
+
+    private LocalDateTime timestamp;
+    private Integer status;
+    private String error;
+    private String path;
+}
