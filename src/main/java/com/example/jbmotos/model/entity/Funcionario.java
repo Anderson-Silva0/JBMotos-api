8be622fde8eb70080b86ca6573a79316de5da71b
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-02-06 18:55:27 -0300

    entity Funcionario alterada

--- src/main/java/com/example/jbmotos/model/entity/Funcionario.java
+++ src/main/java/com/example/jbmotos/model/entity/Funcionario.java
@@ -1,12 +1,9 @@
 package com.example.jbmotos.model.entity;
 
-import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.br.CPF;
 
 import javax.persistence.*;
 
@@ -19,17 +16,16 @@ import javax.persistence.*;
 public class Funcionario {
 
     @Id
-    @Column(name = "cpf", unique = true)
-    @CPF
+    @Column(name = "cpf")
     private String cpf;
 
     @Column(name = "nome")
-    @NotBlank(message = "O campo Nome é obrigatório")
-    @Length(min = 3, max = 50, message = "O campo Nome deve ter entre 3 e 50 caracteres.")
     private String nome;
 
     @Column(name = "telefone")
-    @NotBlank(message = "O campo Telefone é obrigatório")
-    @Length(min = 11, max = 15, message = "O campo Telefone deve ter entre 11 e 15 caracteres.")
     private String telefone;
+
+    @JoinColumn(name = "id_endereco")
+    @OneToOne
+    private Endereco endereco;
 }
