19e31018fed873b13eb31197e884d9e16782a16f
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-02-06 19:03:04 -0300

    classe FuncionarioServiceImpl finalizada

--- src/main/java/com/example/jbmotos/services/impl/FuncionarioServiceImpl.java
+++ src/main/java/com/example/jbmotos/services/impl/FuncionarioServiceImpl.java
@@ -3,14 +3,18 @@ package com.example.jbmotos.services.impl;
 import com.example.jbmotos.api.dto.FuncionarioDTO;
 import com.example.jbmotos.model.entity.Funcionario;
 import com.example.jbmotos.model.repositories.FuncionarioRepository;
+import com.example.jbmotos.services.ClienteService;
+import com.example.jbmotos.services.EnderecoService;
 import com.example.jbmotos.services.FuncionarioService;
+import com.example.jbmotos.services.exception.ObjetoNaoEncontradoException;
+import com.example.jbmotos.services.exception.RegraDeNegocioException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -20,12 +24,24 @@ public class FuncionarioServiceImpl implements FuncionarioService {
     private FuncionarioRepository funcionarioRepository;
 
     @Autowired
+    @Lazy
+    private EnderecoService enderecoService;
+
+    @Autowired
+    @Lazy
+    private ClienteService clienteService;
+
+    @Autowired
     private ModelMapper mapper;
 
     @Override
     @Transactional
     public Funcionario salvarFuncionario(FuncionarioDTO funcionarioDTO) {
-        return funcionarioRepository.save(mapper.map(funcionarioDTO, Funcionario.class));
+        existeFuncionarioPorCpfParaSalvar(funcionarioDTO.getCpf());
+        validaEnderecoParaSalvar(funcionarioDTO);
+        Funcionario funcionario = mapper.map(funcionarioDTO, Funcionario.class);
+        funcionario.setEndereco(enderecoService.buscarEnderecoPorId(funcionarioDTO.getEndereco()).get());
+        return funcionarioRepository.save(funcionario);
     }
 
     @Override
@@ -37,19 +53,66 @@ public class FuncionarioServiceImpl implements FuncionarioService {
     @Override
     @Transactional(readOnly = true)
     public Optional<Funcionario> buscarFuncionarioPorCPF(String cpf) {
+        verificaSeFuncionarioExiste(cpf);
         return funcionarioRepository.findFuncionarioByCpf(cpf);
     }
 
     @Override
     @Transactional
     public Funcionario atualizarFuncionario(FuncionarioDTO funcionarioDTO) {
-        Objects.requireNonNull(funcionarioDTO.getCpf(), "Erro ao tentar atualizar o Funcionário. Informe um CPF.");
-        return funcionarioRepository.save(mapper.map(funcionarioDTO, Funcionario.class));
+        verificaSeFuncionarioExiste(funcionarioDTO.getCpf());
+        validaEnderecoParaAtualizar(funcionarioDTO);
+        Funcionario funcionario = mapper.map(funcionarioDTO, Funcionario.class);
+        funcionario.setEndereco(enderecoService.buscarEnderecoPorId(funcionarioDTO.getEndereco()).get());
+        return funcionarioRepository.save(funcionario);
     }
 
     @Override
     @Transactional
     public void deletarFuncionario(String cpf) {
+        verificaSeFuncionarioExiste(cpf);
         funcionarioRepository.deleteFuncionarioByCpf(cpf);
+    }
+
+    @Override
+    public void validaEnderecoParaSalvar(FuncionarioDTO funcionarioDTO) {
+            buscarTodosFuncionarios().stream().forEach(funcionarioFiltrado -> {
+                if (funcionarioDTO.getEndereco() == funcionarioFiltrado.getEndereco().getId())
+                    throw new RegraDeNegocioException("Erro ao tentar salvar Funcionário," +
+                            " o Endereço já pertence a um Funcionário.");
+            });
+            clienteService.buscarTodosClientes().stream().forEach(c -> {
+                if (funcionarioDTO.getEndereco() == c.getEndereco().getId())
+                    throw new RegraDeNegocioException("Erro ao tentar salvar Funcionário," +
+                            " o Endereço já pertence a um Cliente.");
+            });
+    }
+
+    @Override
+    public void validaEnderecoParaAtualizar(FuncionarioDTO funcionarioDTO) {
+        Funcionario funcionario = buscarFuncionarioPorCPF(funcionarioDTO.getCpf()).get();
+            buscarTodosFuncionarios().stream().filter(f -> funcionario.getCpf() != f.getCpf() )
+                    .forEach(funcionarioFiltrado -> {
+                if (funcionarioDTO.getEndereco() == funcionarioFiltrado.getEndereco().getId())
+                    throw new RegraDeNegocioException("Erro ao tentar atualizar Funcionário," +
+                            " o Endereço já pertence a um Funcionário.");
+            });
+            clienteService.buscarTodosClientes().stream().forEach(c -> {
+                if (funcionarioDTO.getEndereco() == c.getEndereco().getId())
+                    throw new RegraDeNegocioException("Erro ao tentar atualizar Funcionário," +
+                            " o Endereço já pertence a um Cliente.");
+            });
+    }
+
+    @Override
+    public void existeFuncionarioPorCpfParaSalvar(String cpf) {
+        if (funcionarioRepository.existsFuncionarioByCpf(cpf))
+            throw new RegraDeNegocioException("Erro ao tentar salvar Funcionário, CPF já cadastrado.");
+    }
+
+    @Override
+    public void verificaSeFuncionarioExiste(String cpf) {
+        if (!funcionarioRepository.existsFuncionarioByCpf(cpf))
+            throw new ObjetoNaoEncontradoException("Funcionário não encrontrado para o CPF informado.");
     }
 }
