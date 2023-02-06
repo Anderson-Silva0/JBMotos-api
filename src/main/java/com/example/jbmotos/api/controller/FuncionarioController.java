d547fa7d3798458d75b5e9ef73400bb46e7b96ca
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-02-06 18:52:38 -0300

    classe FuncionarioController finalizada

--- src/main/java/com/example/jbmotos/api/controller/FuncionarioController.java
+++ src/main/java/com/example/jbmotos/api/controller/FuncionarioController.java
@@ -1,10 +1,18 @@
 package com.example.jbmotos.api.controller;
 
+import com.example.jbmotos.api.dto.FuncionarioDTO;
+import com.example.jbmotos.model.entity.Funcionario;
 import com.example.jbmotos.services.FuncionarioService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/funcionario")
@@ -16,5 +24,39 @@ public class FuncionarioController {
     @Autowired
     private ModelMapper mapper;
 
+    @PostMapping
+    public ResponseEntity<FuncionarioDTO> salvar(@Valid @RequestBody FuncionarioDTO funcionarioDTO) {
+        Funcionario funcionario = funcionarioService.salvarFuncionario(funcionarioDTO);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest().buildAndExpand( funcionario ).toUri();
+        return ResponseEntity.created(uri).body( mapper.map(funcionario, FuncionarioDTO.class) );
+    }
 
+    @GetMapping("/buscar-todos")
+    public ResponseEntity<List<FuncionarioDTO>> buscarTodos() {
+        return ResponseEntity.ok().body(
+                funcionarioService.buscarTodosFuncionarios().stream().map(funcionario ->
+                        mapper.map(funcionario, FuncionarioDTO.class)
+                        ).collect(Collectors.toList()));
+    }
+
+    @GetMapping("/buscar/{cpf}")
+    public ResponseEntity<FuncionarioDTO> buscarPorCpf(@PathVariable("cpf") String cpf) {
+        return ResponseEntity.ok().body(
+                mapper.map(funcionarioService.buscarFuncionarioPorCPF(cpf), FuncionarioDTO.class));
+    }
+
+    @PostMapping("/atualizar/{cpf}")
+    public ResponseEntity<FuncionarioDTO> atualizar(@PathVariable("cpf") String cpf,
+                                                   @Valid @RequestBody FuncionarioDTO funcionarioDTO) {
+        funcionarioDTO.setCpf(cpf);
+        return ResponseEntity.ok().body(
+                mapper.map(funcionarioService.atualizarFuncionario(funcionarioDTO), FuncionarioDTO.class));
+    }
+
+    @DeleteMapping("/deletar/{cpf}")
+    public ResponseEntity deletar(@PathVariable("cpf") String cpf) {
+        funcionarioService.deletarFuncionario(cpf);
+        return ResponseEntity.noContent().build();
+    }
 }
