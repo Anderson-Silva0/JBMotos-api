f6ffc280b8e661c8cac9215d9c21e3cc3f154dc8
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-02-06 09:52:05 -0300

    mapeamento de Funcionario para FuncionarioDTO criado com ModelMapper

--- src/main/java/com/example/jbmotos/config/ModelMapperConfig.java
+++ src/main/java/com/example/jbmotos/config/ModelMapperConfig.java
@@ -1,7 +1,9 @@
 package com.example.jbmotos.config;
 
 import com.example.jbmotos.api.dto.ClienteDTO;
+import com.example.jbmotos.api.dto.FuncionarioDTO;
 import com.example.jbmotos.model.entity.Cliente;
+import com.example.jbmotos.model.entity.Funcionario;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,7 +16,10 @@ public class ModelMapperConfig {
         var modelMapper = new ModelMapper();
 
         modelMapper.createTypeMap(Cliente.class, ClienteDTO.class)
-                .<Integer>addMapping(src -> src.getEndereco().getId(), (dest, value) -> dest.setEndereco(value));
+                .<Integer>addMapping(src -> src.getEndereco().getId(), (dest, valor) -> dest.setEndereco(valor));
+
+        modelMapper.createTypeMap(Funcionario.class, FuncionarioDTO.class)
+                .<Integer>addMapping(src -> src.getEndereco().getId(), (dest, valor) -> dest.setEndereco(valor));
 
         return modelMapper;
     }
