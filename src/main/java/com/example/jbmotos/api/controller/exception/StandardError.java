6544b4bae57655df0afb1629fad979afb4cd278f
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-02-12 14:22:53 -0300

    alteração em StandardError

--- src/main/java/com/example/jbmotos/api/controller/exception/StandardError.java
+++ src/main/java/com/example/jbmotos/api/controller/exception/StandardError.java
@@ -3,10 +3,12 @@ package com.example.jbmotos.api.controller.exception;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Getter
 public class StandardError {
