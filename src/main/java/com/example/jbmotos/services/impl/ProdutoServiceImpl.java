43ef751e3d015e8c111b193cb0784dfc6677a340
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-02-23 20:54:38 -0300

    Classe ProdutoServiceImpl finalizada

--- src/main/java/com/example/jbmotos/services/impl/ProdutoServiceImpl.java
+++ src/main/java/com/example/jbmotos/services/impl/ProdutoServiceImpl.java
@@ -4,13 +4,13 @@ import com.example.jbmotos.api.dto.ProdutoDTO;
 import com.example.jbmotos.model.entity.Produto;
 import com.example.jbmotos.model.repositories.ProdutoRepository;
 import com.example.jbmotos.services.ProdutoService;
+import com.example.jbmotos.services.exception.ObjetoNaoEncontradoException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -37,19 +37,28 @@ public class ProdutoServiceImpl implements ProdutoService {
     @Override
     @Transactional(readOnly = true)
     public Optional<Produto> buscarProdutoPorId(Integer id) {
+        verificaSeProdutoExiste(id);
         return produtoRepository.findById(id);
     }
 
     @Override
     @Transactional
     public Produto atualizarProduto(ProdutoDTO produtoDTO) {
-        Objects.requireNonNull(produtoDTO.getId(), "Erro ao tentar atualizar o Produto. Informe um Id.");
+        verificaSeProdutoExiste(produtoDTO.getId());
         return produtoRepository.save(mapper.map(produtoDTO, Produto.class));
     }
 
     @Override
     @Transactional
     public void deletarProduto(Integer id) {
+        verificaSeProdutoExiste(id);
         produtoRepository.deleteById(id);
+    }
+
+    @Override
+    public void verificaSeProdutoExiste(Integer id) {
+        if (!produtoRepository.existsById(id)) {
+            throw new ObjetoNaoEncontradoException("Produto não encontrado para o Id informado.");
+        }
     }
 }
