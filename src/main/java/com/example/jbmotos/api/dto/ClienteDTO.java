695bec1aab8f8f3180ed8d34701f7e58a3da3ba4
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-02-23 19:09:45 -0300

    Mensagens de validações alteradas

--- src/main/java/com/example/jbmotos/api/dto/ClienteDTO.java
+++ src/main/java/com/example/jbmotos/api/dto/ClienteDTO.java
@@ -17,21 +17,21 @@ import javax.validation.constraints.NotNull;
 @Builder
 public class ClienteDTO {
 
-    @CPF(message = "Número do CPF inválido")
+    @CPF(message = "Número do CPF inexistente.")
     private String cpf;
 
-    @NotBlank(message = "O campo Nome é obrigatório")
-    @Length(min = 3,max = 50, message = "O campo Nome deve ter entre 3 e 50 caracteres")
+    @NotBlank(message = "O campo Nome é obrigatório.")
+    @Length(min = 3,max = 50, message = "O campo Nome deve ter entre 3 e 50 caracteres.")
     private String nome;
 
-    @NotBlank(message = "O campo Email é obrigatório")
-    @Email(message = "Endereço de E-mail inválido")
+    @NotBlank(message = "O campo Email é obrigatório.")
+    @Email(message = "Endereço de E-mail inválido.")
     private String email;
 
-    @NotBlank(message = "O campo Telefone é obrigatório")
+    @NotBlank(message = "O campo Telefone é obrigatório.")
     @Length(min = 15, max = 15, message = "O campo Telefone está incorreto.")
     private String telefone;
 
-    @NotNull(message = "O campo Endereço é obrigatório")
+    @NotNull(message = "O Endereço está inválido.")
     private Integer endereco;
 }
