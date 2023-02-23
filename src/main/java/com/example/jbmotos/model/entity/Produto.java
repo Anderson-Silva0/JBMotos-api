5969a94064bfece61ebda89761021b960d5956fd
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-02-23 20:55:55 -0300

    Campo Marca adicionado

--- src/main/java/com/example/jbmotos/model/entity/Produto.java
+++ src/main/java/com/example/jbmotos/model/entity/Produto.java
@@ -1,11 +1,9 @@
 package com.example.jbmotos.model.entity;
 
-import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
@@ -23,15 +21,14 @@ public class Produto {
     private Integer id;
 
     @Column(name = "nome")
-    @NotBlank(message = "O campo Nome é obrigatório")
-    @Length(min = 3, max = 50, message = "O campo Nome deve ter entre 3 e 50 caracteres.")
     private String nome;
 
     @Column(name = "preco_venda")
-    @NotBlank(message = "O campo Preço de Venda é obrigatório")
     private BigDecimal preco_venda;
 
     @Column(name = "min_estoque")
-    @NotBlank(message = "O campo Estoque Mínimo é obrigatório")
     private Integer min_estoque;
+
+    @Column(name = "marca")
+    private String marca;
 }
