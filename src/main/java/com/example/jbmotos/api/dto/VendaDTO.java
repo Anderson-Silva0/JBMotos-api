684eae15fc5fe97afc3cef86c6c0006de15dec5d
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-02-23 19:03:17 -0300

    classe VendaDTO finalizada

--- src/main/java/com/example/jbmotos/api/dto/VendaDTO.java
+++ src/main/java/com/example/jbmotos/api/dto/VendaDTO.java
@@ -1,10 +1,12 @@
 package com.example.jbmotos.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.br.CPF;
 
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
@@ -17,19 +19,22 @@ public class VendaDTO {
 
     private Integer id;
 
-    @NotBlank(message = "O campo Cliente é obrigatório")
-    private String cliente;
+    @NotBlank(message = "O campo Cliente é obrigatório.")
+    @CPF(message = "Número do CPF inexistente.")
+    private String cpf_cliente;
 
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
     private LocalDateTime data_hora;
 
-    @NotBlank(message = "O campo Funcionário é obrigatório")
-    private String funcionario;
+    @NotBlank(message = "O campo Funcionário é obrigatório.")
+    @CPF(message = "Número do CPF inexistente.")
+    private String cpf_funcionario;
 
-    @NotBlank(message = "O campo Observação é obrigatório")
+    @NotBlank(message = "O campo Observação é obrigatório.")
     @Length(max = 255, message = "O campo Observação tem no máximo 255 caracteres.")
     private String observacao;
 
-    @NotBlank(message = "O campo Forma de Pagamento é obrigatório")
+    @NotBlank(message = "O campo Forma de Pagamento é obrigatório.")
     @Length(max = 50, message = "O campo Forma de Pagamento tem no máximo 50 caracteres.")
     private String forma_de_pagamento;
 }
