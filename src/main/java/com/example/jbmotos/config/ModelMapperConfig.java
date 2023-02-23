afb37cc1b28f24c34df39f9493ccd40e7f1d1a0b
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-02-23 19:01:14 -0300

    ModelMapperConfig alterado

--- src/main/java/com/example/jbmotos/config/ModelMapperConfig.java
+++ src/main/java/com/example/jbmotos/config/ModelMapperConfig.java
@@ -2,8 +2,10 @@ package com.example.jbmotos.config;
 
 import com.example.jbmotos.api.dto.ClienteDTO;
 import com.example.jbmotos.api.dto.FuncionarioDTO;
+import com.example.jbmotos.api.dto.VendaDTO;
 import com.example.jbmotos.model.entity.Cliente;
 import com.example.jbmotos.model.entity.Funcionario;
+import com.example.jbmotos.model.entity.Venda;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +22,10 @@ public class ModelMapperConfig {
 
         modelMapper.createTypeMap(Funcionario.class, FuncionarioDTO.class)
                 .<Integer>addMapping(src -> src.getEndereco().getId(), (dest, valor) -> dest.setEndereco(valor));
+
+        modelMapper.createTypeMap(Venda.class, VendaDTO.class)
+                .<String>addMapping(src -> src.getCliente().getCpf(), (dest, valor) -> dest.setCpf_cliente(valor))
+                .<String>addMapping(src -> src.getFuncionario().getCpf(), (dest, valor) -> dest.setCpf_funcionario(valor));
 
         return modelMapper;
     }
