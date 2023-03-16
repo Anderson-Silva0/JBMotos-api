3759601e02340a47f57bf76dfcd7198fb9ca5277
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-03-15 23:32:36 -0300

    Interface ProdutoPedidoService finalizada

--- src/main/java/com/example/jbmotos/services/ProdutoPedidoService.java
+++ src/main/java/com/example/jbmotos/services/ProdutoPedidoService.java
@@ -3,6 +3,7 @@ package com.example.jbmotos.services;
 import com.example.jbmotos.api.dto.ProdutoPedidoDTO;
 import com.example.jbmotos.model.entity.Pedido;
 import com.example.jbmotos.model.entity.ProdutoPedido;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -12,15 +13,19 @@ public interface ProdutoPedidoService {
 
     ProdutoPedido salvarProdutoPedido(ProdutoPedidoDTO produtoPedidoDTO);
 
-    List<ProdutoPedido> buscarProdutosDoPedido(Integer id);
-
-    BigDecimal valorTotalDoPedido(Integer idPedido);
+    List<ProdutoPedido> buscarTodosProdutoPedido();
 
     Optional<ProdutoPedido> buscarProdutoPedidoPorId(Integer id);
 
     ProdutoPedido atualizarProdutoPedido(ProdutoPedidoDTO produtoPedidoDTO);
 
-    void deletarProdutoPedido(Integer id);
+    void deletarProdutoPedidoPorId(Integer id);
+
+    List<ProdutoPedido> buscarProdutosDoPedido(Integer id);
+
+    BigDecimal valorTotalDoPedido(Integer idPedido);
 
     void validaProdutoPedido(Integer id);
+
+    ProdutoPedido getProdutoPedido(ProdutoPedidoDTO produtoPedidoDTO);
 }
