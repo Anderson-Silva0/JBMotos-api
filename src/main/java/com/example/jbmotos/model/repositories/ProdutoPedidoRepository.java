cd14292b9e159fd82df95c176743aac4aed99734
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-03-15 23:36:38 -0300

    Interface ProdutoPedidoRepository finalizada

--- src/main/java/com/example/jbmotos/model/repositories/ProdutoPedidoRepository.java
+++ src/main/java/com/example/jbmotos/model/repositories/ProdutoPedidoRepository.java
@@ -4,7 +4,9 @@ import com.example.jbmotos.model.entity.ProdutoPedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProdutoPedidoRepository extends JpaRepository<ProdutoPedido, Integer> {
-
+    List<ProdutoPedido> findProdutoPedidoByPedidoId(Integer id);
 }
