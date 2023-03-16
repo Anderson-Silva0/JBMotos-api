0c45ebe2d49eae168eb00c780415891b6330bc6e
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-03-15 23:35:09 -0300

    Entity ProdutoPedido finalizada

--- src/main/java/com/example/jbmotos/model/entity/ProdutoPedido.java
+++ src/main/java/com/example/jbmotos/model/entity/ProdutoPedido.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.math.BigDecimal;
 
-@Table(name = "pedido_produto", schema = "jbmotos")
+@Table(name = "produto_pedido", schema = "jbmotos")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,16 +20,23 @@ public class ProdutoPedido {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     @ManyToOne
     @JoinColumn(name = "id_pedido")
     @JsonIgnore
     private Pedido pedido;
+
     @ManyToOne
     @JoinColumn(name = "id_produto")
     @JsonIgnore
     private Produto produto;
 
+    @Column(name = "quantidade")
     private Integer quantidade;
 
-    private BigDecimal valor;
+    @Column(name = "valor_unidade")
+    private BigDecimal valorUnidade;
+
+    @Column(name = "valorTotal")
+    private BigDecimal valorTotal;
 }
