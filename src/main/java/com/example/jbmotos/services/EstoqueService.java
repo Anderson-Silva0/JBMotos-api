6d134ab4819384dad20fb8c3f56b0e4998095c02
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-03-25 22:22:54 -0300

    Interface EstoqueService finalizada

--- src/main/java/com/example/jbmotos/services/EstoqueService.java
+++ src/main/java/com/example/jbmotos/services/EstoqueService.java
@@ -3,20 +3,29 @@ package com.example.jbmotos.services;
 import com.example.jbmotos.api.dto.EstoqueDTO;
 import com.example.jbmotos.model.entity.Estoque;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
 public interface EstoqueService {
 
-    Estoque salvarEndereco(EstoqueDTO estoqueDTO);
+    Estoque salvarEstoque(EstoqueDTO estoqueDTO);
 
-    List<Estoque> buscarTodosEndereco();
+    List<Estoque> buscarTodosEstoques();
 
-    Optional<Estoque> buscarEnderecoPorId(Integer id);
+    Optional<Estoque> buscarEstoquePorId(Integer id);
 
     Estoque atualizarEstoque(EstoqueDTO estoqueDTO);
 
     void deletarEstoquePorId(Integer id);
 
+    Integer obterQuantidadeDoProduto(Integer idProduto);
+
+    void adicionarQuantidadeAoEstoque(Integer idProduto, Integer quantidade);
+
+    BigDecimal calcularValorTotalEstoque();
+
     void validarEstoque(Integer id);
+
+    void verificarUsoEstoque(Integer id);
 }
