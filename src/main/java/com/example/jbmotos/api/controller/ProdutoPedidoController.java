91efdeacfacc3a28586b908103360d95f8c8ef31
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-03-25 22:56:38 -0300

    Classe ProdutoPedidoController finalizada

--- src/main/java/com/example/jbmotos/api/controller/ProdutoPedidoController.java
+++ src/main/java/com/example/jbmotos/api/controller/ProdutoPedidoController.java
@@ -6,17 +6,19 @@ import com.example.jbmotos.services.ProdutoPedidoService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
-import java.math.BigDecimal;
+import javax.validation.constraints.Positive;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/produtopedido")
+@Validated
 public class ProdutoPedidoController {
 
     @Autowired
@@ -26,15 +28,15 @@ public class ProdutoPedidoController {
     private ModelMapper mapper;
 
     @PostMapping
-    public ResponseEntity<ProdutoPedidoDTO> salvar(@Valid @RequestBody ProdutoPedidoDTO produtoPedidoDTO) {
+    public ResponseEntity<ProdutoPedidoDTO> salvar(@Valid @RequestBody ProdutoPedidoDTO produtoPedidoDTO){
         ProdutoPedido produtoPedido = produtoPedidoService.salvarProdutoPedido(produtoPedidoDTO);
         URI uri = ServletUriComponentsBuilder
-                .fromCurrentRequest().buildAndExpand( produtoPedido ).toUri();
+                .fromCurrentRequest().buildAndExpand(produtoPedido).toUri();
         return ResponseEntity.created(uri).body(mapper.map(produtoPedido, ProdutoPedidoDTO.class));
     }
 
     @GetMapping("/buscar-todos")
-    public ResponseEntity<List<ProdutoPedidoDTO>> buscarTodos() {
+    public ResponseEntity<List<ProdutoPedidoDTO>> buscarTodos(){
         return ResponseEntity.ok().body(
                 produtoPedidoService.buscarTodosProdutoPedido().stream().map(produtoPedido ->
                         mapper.map(produtoPedido, ProdutoPedidoDTO.class)
@@ -42,35 +44,38 @@ public class ProdutoPedidoController {
     }
 
     @GetMapping("/buscar/{id}")
-    public ResponseEntity<ProdutoPedidoDTO> buscarPorId(@PathVariable("id") Integer id) {
+    public ResponseEntity<ProdutoPedidoDTO> buscarPorId(@PathVariable("id") Integer id){
         return ResponseEntity.ok().body(mapper.map(produtoPedidoService.
                 buscarProdutoPedidoPorId(id), ProdutoPedidoDTO.class));
     }
 
     @PutMapping("/atualizar/{id}")
     public ResponseEntity<ProdutoPedidoDTO> atualizar(@PathVariable("id") Integer id,
-                                                      @Valid @RequestBody ProdutoPedidoDTO produtoPedidoDTO) {
-        produtoPedidoDTO.setId(id);
-        return ResponseEntity.ok().body(mapper.map(produtoPedidoService
-                .atualizarProdutoPedido(produtoPedidoDTO), ProdutoPedidoDTO.class));
+                                                      @RequestParam Integer idPedido,
+                                                                    Integer idProduto,
+        @Positive(message = "A quantidade deve ser maior que zero") Integer quantidade) {
+        ProdutoPedidoDTO produtoPedidoDTO = ProdutoPedidoDTO.builder()
+                .id(id)
+                .idPedido(idPedido)
+                .idProduto(idProduto)
+                .quantidade(quantidade)
+                .build();
+        return ResponseEntity.ok().body(
+                mapper.map(produtoPedidoService.atualizarProdutoPedido(produtoPedidoDTO), ProdutoPedidoDTO.class)
+        );
     }
 
     @DeleteMapping("/deletar/{id}")
-    public ResponseEntity deletar(@PathVariable("id") Integer id) {
+    public ResponseEntity deletar(@PathVariable("id") Integer id){
         produtoPedidoService.deletarProdutoPedidoPorId(id);
         return ResponseEntity.noContent().build();
     }
 
-    @GetMapping("/buscar-todos-por-pedido/{id}")
-    public ResponseEntity<List<ProdutoPedidoDTO>> buscarTodosPorPedido(@PathVariable("id") Integer idPedido) {
+    @GetMapping("/produtos-pedido/{idPedido}")
+    public ResponseEntity<List<ProdutoPedidoDTO>> buscarTodosPorIdPedido(@PathVariable("idPedido") Integer idPedido){
         return ResponseEntity.ok().body(
-                produtoPedidoService.buscarProdutosDoPedido(idPedido).stream().map(pedido ->
+                produtoPedidoService.buscarProdutoPedidoPorIdPedido(idPedido).stream().map(pedido ->
                         mapper.map(pedido, ProdutoPedidoDTO.class)
                 ).collect(Collectors.toList()));
-    }
-
-    @GetMapping("/valor-total-pedido/{id}")
-    public ResponseEntity<BigDecimal> valorTotalDoPedido(@PathVariable("id") Integer idPedido) {
-        return ResponseEntity.ok().body( produtoPedidoService.valorTotalDoPedido(idPedido) );
     }
 }
