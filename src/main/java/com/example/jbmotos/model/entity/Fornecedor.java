16a123895b3638dd1eb35af793e170d194a195c7
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-03-29 21:46:29 -0300

    Entity Fornecedor alterada e finalizada.

--- src/main/java/com/example/jbmotos/model/entity/Fornecedor.java
+++ src/main/java/com/example/jbmotos/model/entity/Fornecedor.java
@@ -1,14 +1,14 @@
 package com.example.jbmotos.model.entity;
 
 import javax.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.br.CNPJ;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Table(name = "fornecedor", schema = "jbmotos")
 @Data
@@ -21,16 +21,26 @@ public class Fornecedor {
     @Id
     @Column(name = "cnpj", unique = true)
     @NotBlank(message = "O campo CNPJ é obrigatório")
+    @Length(max = 18)
     @CNPJ
     private String cnpj;
 
     @Column(name = "nome")
     @NotBlank(message = "O campo Nome é obrigatório")
-    @Length(min = 3, max = 50, message = "O campo Nome deve ter entre 3 e 50 caracteres.")
+    @Length(max = 50)
     private String nome;
 
     @Column(name = "telefone")
     @NotBlank(message = "O campo Telefone é obrigatório")
-    @Length(min =11, max = 15, message = "O campo Telefone deve ter entre 11 e 15 caracteres.")
+    @Length(max = 15)
     private String telefone;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_endereco", referencedColumnName = "id")
+    private Endereco endereco;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "fornecedor")
+    @ToString.Exclude
+    @JsonIgnore
+    private List<Produto> produtos;
 }
