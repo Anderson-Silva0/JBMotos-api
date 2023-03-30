0dd118ead48d358f3a55eb8017e08262a0221533
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-03-29 21:49:04 -0300

    Interface FornecedorRepository alterada e finalizada.

--- src/main/java/com/example/jbmotos/model/repositories/FornecedorRepository.java
+++ src/main/java/com/example/jbmotos/model/repositories/FornecedorRepository.java
@@ -4,6 +4,7 @@ import com.example.jbmotos.model.entity.Fornecedor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,5 +12,11 @@ public interface FornecedorRepository extends JpaRepository<Fornecedor, Integer>
 
     Optional<Fornecedor> findFornecedorByCnpj(String cnpj);
 
+    boolean existsFornecedorByCnpj(String cnpj);
+
     void deleteFornecedorByCnpj(String cnpj);
+
+    boolean existsFornecedorByEnderecoId(Integer idEndereco);
+
+    List<Fornecedor> findByCnpjNot(String cnpj);
 }
