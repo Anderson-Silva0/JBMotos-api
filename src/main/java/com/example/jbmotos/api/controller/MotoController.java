c6dc609be7c3856f71d7f4fdf1de699ca8227b96
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-03-31 19:42:41 -0300

    Classe MotoController finalizada

--- src/main/java/com/example/jbmotos/api/controller/MotoController.java
+++ src/main/java/com/example/jbmotos/api/controller/MotoController.java
@@ -6,14 +6,13 @@ import com.example.jbmotos.services.MotoService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/moto")
@@ -31,5 +30,50 @@ public class MotoController {
         URI uri = ServletUriComponentsBuilder
                 .fromCurrentRequest().buildAndExpand(moto).toUri();
         return ResponseEntity.created(uri).body(mapper.map(moto, MotoDTO.class));
+    }
+
+    @GetMapping("/buscar-todas")
+    public ResponseEntity<List<MotoDTO>> buscarTodas() {
+        return ResponseEntity.ok().body(
+                motoService.buscarTodasMotos().stream().map(moto ->
+                        mapper.map(moto, MotoDTO.class)
+                ).collect(Collectors.toList()));
+    }
+
+    @GetMapping("/buscar-por-cpf/{cpfCliente}")
+    public ResponseEntity<List<MotoDTO>> buscarMotosPorCpfCliente(@PathVariable("cpfCliente") String cpfCliente) {
+        return ResponseEntity.ok().body(
+                motoService.buscarMotosPorCpfCliente(cpfCliente).stream().map(moto ->
+                        mapper.map(moto, MotoDTO.class)
+                ).collect(Collectors.toList()));
+    }
+
+    @GetMapping("/buscar-por-id/{idMoto}")
+    public ResponseEntity<MotoDTO> buscarPorId(@PathVariable("idMoto") Integer idMoto) {
+        return ResponseEntity.ok().body(mapper.map(motoService.buscarMotoPorId(idMoto), MotoDTO.class));
+    }
+
+    @GetMapping("/buscar-por-placa/{placa}")
+    public ResponseEntity<MotoDTO> buscarPorPlaca(@PathVariable("placa") String placa) {
+        return ResponseEntity.ok().body(mapper.map(motoService.buscarMotoPorPlaca(placa), MotoDTO.class));
+    }
+
+    @PutMapping("/atualizar/{idMoto}")
+    public ResponseEntity<MotoDTO> atualizar(@PathVariable("idMoto") Integer idMoto,
+                                             @Valid @RequestBody MotoDTO motoDTO) {
+        motoDTO.setId(idMoto);
+        return ResponseEntity.ok().body(mapper.map(motoService.atualizarMoto(motoDTO), MotoDTO.class));
+    }
+
+    @DeleteMapping("/deletar-por-id/{idMoto}")
+    public ResponseEntity deletarPorId(@PathVariable("idMoto") Integer idMoto) {
+        motoService.deletarMotoPorId(idMoto);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/deletar-por-placa/{placa}")
+    public ResponseEntity deletarPorPlaca(@PathVariable("placa") String placa) {
+        motoService.deletarMotoPorPlaca(placa);
+        return ResponseEntity.noContent().build();
     }
 }
