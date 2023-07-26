4ae1cc2fcc1864ba4f82dd0521943fcab26b6c8a
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-07-26 15:38:39 -0300

    "Campo statusCliente adicionado e alteração nas validações em ClienteDTO"

--- src/main/java/com/example/jbmotos/api/dto/ClienteDTO.java
+++ src/main/java/com/example/jbmotos/api/dto/ClienteDTO.java
@@ -8,11 +8,9 @@ import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.br.CPF;
 
-
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import java.time.LocalDateTime;
 
 @Data
@@ -27,17 +25,19 @@ public class ClienteDTO {
     private String cpf;
 
     @NotBlank(message = "O campo Nome é obrigatório.")
-    @Length(min = 3,max = 50, message = "O campo Nome deve ter entre 3 e 50 caracteres.")
+    @Length(min = 3, max = 50, message = "O campo Nome deve ter entre 3 e 50 caracteres.")
     private String nome;
 
     @NotBlank(message = "O campo Email é obrigatório.")
-    @Length(min = 5,max = 200, message = "O campo Email deve ter entre 5 e 200 caracteres.")
+    @Length(min = 5, max = 200, message = "O campo Email deve ter entre 5 e 200 caracteres.")
     @Email(message = "O endereço de email informado é inválido.")
     private String email;
 
     @NotBlank(message = "O campo Telefone é obrigatório.")
     @Length(min = 15, max = 15, message = "O campo Celular deve ter no mínimo 15 caracteres.")
     private String telefone;
+
+    private String statusCliente;
 
     @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
     private LocalDateTime dataHoraCadastro;
