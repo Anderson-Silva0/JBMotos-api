7d297ea34e5c95ca3bcf0ca0276bdfc7d31fbea8
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-09-07 23:10:36 -0300

    Adaptação de mapeamentos para DTOs e entidades.

--- src/main/java/com/example/jbmotos/config/ModelMapperConfig.java
+++ src/main/java/com/example/jbmotos/config/ModelMapperConfig.java
@@ -1,34 +1,43 @@
 package com.example.jbmotos.config;
 
-import com.example.jbmotos.api.dto.*;
-import com.example.jbmotos.model.entity.*;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.example.jbmotos.api.dto.ClienteDTO;
+import com.example.jbmotos.api.dto.FornecedorDTO;
+import com.example.jbmotos.api.dto.FuncionarioDTO;
+import com.example.jbmotos.api.dto.PedidoDTO;
+import com.example.jbmotos.api.dto.ProdutoPedidoDTO;
+import com.example.jbmotos.model.entity.Cliente;
+import com.example.jbmotos.model.entity.Fornecedor;
+import com.example.jbmotos.model.entity.Funcionario;
+import com.example.jbmotos.model.entity.Pedido;
+import com.example.jbmotos.model.entity.ProdutoPedido;
 
 @Configuration
 public class ModelMapperConfig {
 
     @Bean
-    public ModelMapper ModelMapper() {
+    ModelMapper ModelMapper() {
         var modelMapper = new ModelMapper();
 
         modelMapper.createTypeMap(Cliente.class, ClienteDTO.class)
-                .<Integer>addMapping(src -> src.getEndereco().getId(), (dest, valor) -> dest.setEndereco(valor));
+                .<Integer>addMapping(src -> src.getEndereco().getId(), ClienteDTO::setEndereco);
 
         modelMapper.createTypeMap(Funcionario.class, FuncionarioDTO.class)
-                .<Integer>addMapping(src -> src.getEndereco().getId(), (dest, valor) -> dest.setEndereco(valor));
+                .<Integer>addMapping(src -> src.getEndereco().getId(), FuncionarioDTO::setEndereco);
 
         modelMapper.createTypeMap(Fornecedor.class, FornecedorDTO.class)
-                        .<Integer>addMapping(src -> src.getEndereco().getId(), (dest, valor) -> dest.setEndereco(valor));
+                        .<Integer>addMapping(src -> src.getEndereco().getId(), FornecedorDTO::setEndereco);
 
         modelMapper.createTypeMap(Pedido.class, PedidoDTO.class)
-                .<String>addMapping(src -> src.getCliente().getCpf(), (dest, valor) -> dest.setCpfCliente(valor))
-                .<String>addMapping(src -> src.getFuncionario().getCpf(), (dest, valor) -> dest.setCpfFuncionario(valor));
+                .<String>addMapping(src -> src.getCliente().getCpf(), PedidoDTO::setCpfCliente)
+                .<String>addMapping(src -> src.getFuncionario().getCpf(), PedidoDTO::setCpfFuncionario);
 
         modelMapper.createTypeMap(ProdutoPedido.class, ProdutoPedidoDTO.class)
-                .<Integer>addMapping(src -> src.getProduto().getId(), (dest, valor) -> dest.setIdProduto(valor))
-                .<Integer>addMapping(src -> src.getPedido().getId(), (dest, valor) -> dest.setIdPedido(valor));
+                .<Integer>addMapping(src -> src.getProduto().getId(), ProdutoPedidoDTO::setIdProduto)
+                .<Integer>addMapping(src -> src.getPedido().getId(), ProdutoPedidoDTO::setIdPedido);
 
         return modelMapper;
     }
