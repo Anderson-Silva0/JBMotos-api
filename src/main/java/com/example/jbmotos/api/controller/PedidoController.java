c0d4ba2a16a2a8579986107813ce5d4dedcdb593
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-09-07 22:45:16 -0300

    Adição de endpoint de filtragem, renomeação de método e ajustes de organização de código no arquivo PedidoController.java.

--- src/main/java/com/example/jbmotos/api/controller/PedidoController.java
+++ src/main/java/com/example/jbmotos/api/controller/PedidoController.java
@@ -1,20 +1,30 @@
 package com.example.jbmotos.api.controller;
 
-import com.example.jbmotos.api.dto.PedidoDTO;
-import com.example.jbmotos.api.dto.ProdutoDTO;
-import com.example.jbmotos.model.entity.Pedido;
-import com.example.jbmotos.services.PedidoService;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import javax.validation.Valid;
 import java.math.BigDecimal;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.example.jbmotos.api.dto.PedidoDTO;
+import com.example.jbmotos.api.dto.ProdutoDTO;
+import com.example.jbmotos.model.entity.Pedido;
+import com.example.jbmotos.services.PedidoService;
 
 @RestController
 @RequestMapping("/api/pedido")
@@ -47,6 +57,21 @@ public class PedidoController {
         return ResponseEntity.ok().body(mapper.map(pedidoService.buscarPedidoPorId(id).get(), PedidoDTO.class));
     }
 
+    @GetMapping("/filtrar")
+    public ResponseEntity<List<PedidoDTO>> filtrar(
+            @RequestParam(value = "cpfCliente", required = false) String cpfCliente,
+            @RequestParam(value = "cpfFuncionario", required = false) String cpfFuncionario
+    ) {
+        PedidoDTO pedidoDTO = PedidoDTO.builder()
+                .cpfCliente(cpfCliente)
+                .cpfFuncionario(cpfFuncionario)
+                .build();
+        return ResponseEntity.ok().body(
+                pedidoService.filtrarPedido(pedidoDTO).stream().map(pedido ->
+                        mapper.map(pedido, PedidoDTO.class)
+                ).collect(Collectors.toList()));
+    }
+
     @PutMapping("/atualizar/{id}")
     public ResponseEntity<PedidoDTO> atualizar(@PathVariable("id") Integer id,
                                                @Valid @RequestBody PedidoDTO pedidoDTO) {
@@ -55,13 +80,13 @@ public class PedidoController {
     }
 
     @DeleteMapping("/deletar/{id}")
-    public ResponseEntity deletar(@PathVariable("id") Integer id) {
+    public ResponseEntity<?> deletar(@PathVariable("id") Integer id) {
         pedidoService.deletarPedido(id);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/lucro-pedido/{idPedido}")
-    public ResponseEntity<BigDecimal> LucroDoPedido(@PathVariable("idPedido") Integer idPedido) {
+    public ResponseEntity<BigDecimal> lucroDoPedido(@PathVariable("idPedido") Integer idPedido) {
         return ResponseEntity.ok().body(pedidoService.calcularLucroDoPedido(idPedido));
     }
 
