5517d4edfc41d8fc642445f47ac5506f6204ba20
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-09-07 22:41:39 -0300

    Adição de endpoints de filtro, atualização de retorno no método alternarStatus e otimizações na organização do código no FuncionarioController

--- src/main/java/com/example/jbmotos/api/controller/FuncionarioController.java
+++ src/main/java/com/example/jbmotos/api/controller/FuncionarioController.java
@@ -1,19 +1,30 @@
 package com.example.jbmotos.api.controller;
 
-import com.example.jbmotos.api.dto.FuncionarioDTO;
-import com.example.jbmotos.model.entity.Funcionario;
-import com.example.jbmotos.model.enums.StatusFuncionario;
-import com.example.jbmotos.services.FuncionarioService;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.example.jbmotos.api.dto.FuncionarioDTO;
+import com.example.jbmotos.model.entity.Funcionario;
+import com.example.jbmotos.model.enums.Situacao;
+import com.example.jbmotos.services.FuncionarioService;
 
 @RestController
 @RequestMapping("/api/funcionario")
@@ -51,12 +62,14 @@ public class FuncionarioController {
     public ResponseEntity<List<FuncionarioDTO>> filtrar(
             @RequestParam(value = "cpf", required = false) String cpf,
             @RequestParam(value = "nome", required = false) String nome,
-            @RequestParam(value = "telefone", required = false) String telefone
+            @RequestParam(value = "telefone", required = false) String telefone,
+            @RequestParam(value = "statusFuncionario", required = false) String statusFuncionario
     ) {
         FuncionarioDTO funcionarioDTO = FuncionarioDTO.builder()
                 .cpf(cpf)
                 .nome(nome)
                 .telefone(telefone)
+                .statusFuncionario(statusFuncionario)
                 .build();
         return ResponseEntity.ok().body(
                 funcionarioService.filtrarFuncionario(funcionarioDTO).stream().map(funcionario ->
@@ -65,8 +78,8 @@ public class FuncionarioController {
     }
 
     @PatchMapping("/alternar-status/{cpf}")
-    public ResponseEntity<StatusFuncionario> alternarStatus(@PathVariable("cpf") String cpf) {
-        StatusFuncionario statusFuncionario = funcionarioService.alternarStatusFuncionario(cpf);
+    public ResponseEntity<Situacao> alternarStatus(@PathVariable("cpf") String cpf) {
+    	Situacao statusFuncionario = funcionarioService.alternarStatusFuncionario(cpf);
         return ResponseEntity.ok().body(statusFuncionario);
     }
 
@@ -79,7 +92,7 @@ public class FuncionarioController {
     }
 
     @DeleteMapping("/deletar/{cpf}")
-    public ResponseEntity deletar(@PathVariable("cpf") String cpf) {
+    public ResponseEntity<?> deletar(@PathVariable("cpf") String cpf) {
         funcionarioService.deletarFuncionario(cpf);
         return ResponseEntity.noContent().build();
     }
