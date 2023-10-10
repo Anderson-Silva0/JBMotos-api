f10526d70706fb45505fb568808f7effc3c3f942
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-10-10 20:47:34 -0300

    Classe PagamentoCartaoDTO criada

---
+++ src/main/java/com/jbmotos/api/dto/PagamentoCartaoDTO.java
@@ -0,0 +1,33 @@
+package com.jbmotos.api.dto;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class PagamentoCartaoDTO {
+
+    private Integer id;
+
+    @NotNull(message = "Informe a Quantidade de Parcelas.")
+    private String parcela;
+
+    @NotNull(message = "Informe a Bandeira do Cartão.")
+    private String bandeira;
+
+    @NotNull(message = "Informe o Total de Taxas.")
+    @DecimalMin(value = "0.0", inclusive = false, message = "O Total de Taxas deve ser maior que zero.")
+    private BigDecimal totalTaxas;
+
+    @NotNull(message = "O Id da Venda não pode ser nulo.")
+    private Integer idVenda;
+}
