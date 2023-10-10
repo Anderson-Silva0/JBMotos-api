01e992faea08cbf027a75af916c98fa6693796e0
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-10-10 20:42:56 -0300

    Entidade PagamentoCartao criada

---
+++ src/main/java/com/jbmotos/model/entity/PagamentoCartao.java
@@ -0,0 +1,32 @@
+package com.jbmotos.model.entity;
+
+import jakarta.persistence.*;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "pagamento_cartao", schema = "jbmotos")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class PagamentoCartao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String parcela;
+
+    private String bandeira;
+
+    private BigDecimal totalTaxas;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_venda", referencedColumnName = "id")
+    private Venda venda;
+}
