4c316581ee40d8e4922eaa04d5456275b79027a9
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2024-01-25 21:21:12 -0300

    adicionado método findByVendaId

--- src/main/java/com/jbmotos/model/repositories/PagamentoCartaoRepository.java
+++ src/main/java/com/jbmotos/model/repositories/PagamentoCartaoRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 public interface PagamentoCartaoRepository extends JpaRepository<PagamentoCartao, Integer> {
 
     boolean existsByVendaId(Integer idVenda);
+
+    Optional<PagamentoCartao> findByVendaId(Integer idVenda);
 }
