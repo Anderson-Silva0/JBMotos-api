fb16770f35fba4f55d8ac7d4fac27ecf6c5e4b14
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2024-09-10 21:57:08 -0300

    adicionado url para permição da vercel

--- src/main/java/com/jbmotos/config/WebConfig.java
+++ src/main/java/com/jbmotos/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings( CorsRegistry registry ) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://jb-motos-client-fj7vietvc-andersonsilva0s-projects.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
